cea83b1ba67f7dd9a870a1f9ff14d6ca678a904f
Dzung Tran <xuandung.hp.vn@gmail.com>
2015-10-18 14:35:44 +0700

    Initial Commit

---
+++ TableViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableViewDemo
+//
+//  Created by Dzung Tran on 10/18/15.
+//  Copyright © 2015 Dzung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableViewDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TableViewDemo
+//
+//  Created by Dzung Tran on 10/18/15.
+//  Copyright © 2015 Dzung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
